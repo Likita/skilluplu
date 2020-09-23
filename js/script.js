5e8941c4b12110bf588af3e48a6b340bf1158e24
--global <arturakk2015@gmail.com>
2020-09-23 20:08:23 +0300

    added: - added anchors (with animated scroll) - meta tags for index3.html - added margins for .technology - min font-size 18px - added margins for .learning

--- js/script.js
+++ js/script.js
@@ -36,3 +36,19 @@ for (let i = 0; i < question.length; i++) {
     answer[i].classList.toggle('active')
   })
 }
+
+// animated scroll for anchors
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
