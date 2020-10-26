cae8e3182a150f5eacb2c360e1796861215be880
Likita <vasilika-ne@yandex.ru>
2020-10-26 01:23:57 +0100

    Popups, privacy policy

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,26 @@
-const enterEmailBtn = document.querySelectorAll('.enter-email')
-const popup = document.getElementById('popup')
-const body = document.querySelector('.body')
-const closePopup = document.querySelector('.popup__close')
+const openPopupBtn = document.querySelectorAll('.enter-email');
+const discountPopup = document.getElementById('discount-popup');
+const signupPopup = document.getElementById('signup-popup');
+const body = document.querySelector('.body');
+const closePopup = document.querySelectorAll('.popup__close');
 
-for(let i = 0; i < enterEmailBtn.length; i++) {
-  enterEmailBtn[i].addEventListener('click', () => {
-    popup.classList.remove('visually-hidden')
-    body.classList.add('body__overlay')
+for(let i = 0; i < openPopupBtn.length; i++) {
+  openPopupBtn[i].addEventListener('click', () => {
+    signupPopup.classList.remove('visually-hidden');
+    body.classList.add('body__overlay');
+    signupPopup.querySelector('input').focus();
   })
 }
 
-closePopup.addEventListener('click', () => {
-  popup.classList.add('visually-hidden')
-    body.classList.remove('body__overlay')
-})
+for(let i = 0; i < closePopup.length; i++) {
+  closePopup[i].addEventListener('click', () => {
+    closePopup[i].parentElement.classList.add('visually-hidden');
+      body.classList.remove('body__overlay');
+  })
+}
+
+setTimeout(function() {
+  discountPopup.classList.remove('visually-hidden');
+  body.classList.add('body__overlay');
+  discountPopup.querySelector('input').focus();
+}, 40000)
